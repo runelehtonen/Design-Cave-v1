c38ee8bb1a31cfdca096ef3863c3458b18c2ccee
runelehtonen <71000750+runelehtonen@users.noreply.github.com>
2021-05-28 03:16:09 +0200

    -

--- js/js.js
+++ js/js.js
@@ -15,8 +15,7 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-} 
-
+}
 
 
 
